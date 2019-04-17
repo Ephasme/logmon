5f10bc4f6c0abdc971a9a649aace0e428a599cc3
Loup Peluso <loup.peluso@gmail.com>
2019-04-17 22:32:40 +0200

    Cleanup of index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,12 @@ import { defaultState } from "./Stats/defaultStates";
 const args = yargs.default("filename", "/tmp/access.log").argv;
 
 const watcher = kernel.createLogWatcher(args.filename);
-
 const each10seconds = kernel.createTimerMonitor(10000);
-const eachSecond = kernel.createTimerMonitor(1000);
 
 console.log("Application started:");
 console.log(`   * watching ${args.filename}`);
 
 watcher.subscribe(each10seconds);
-watcher.subscribe(eachSecond);
 
 let currentState = defaultState;
 
@@ -23,13 +20,6 @@ each10seconds.run((batch: ILogLine[]) => {
     currentState = mainReducer(currentState, batch);
     if (currentState.hasChanged) {
         console.log(JSON.stringify(currentState, null, 4));
-    }
-});
-
-eachSecond.run((batch: ILogLine[]) => {
-    const alert = batch.length > 10;
-    if (alert) {
-        console.log("start alert");
     }
 });
 
